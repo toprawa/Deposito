5bfb430812f6c55db5dbad34872ee4a2007701d6
LMEL <lucasescar@gmail.com>
2022-02-28 00:45:12 +0100

    Comentario para GIT commit

--- Deposito/src/cuentas/CCuenta.java
+++ Deposito/src/cuentas/CCuenta.java
@@ -1,10 +1,14 @@
+/**
+ * Comentario para GIT commit
+ */
+package cuentas;
 public class CCuenta {
 
 
     private String nombre;
     private String cuenta;
     private double saldo;
-    private double tipoInterÃ©s;
+    private double tipoInterés;
 
     public CCuenta()
     {
@@ -12,21 +16,21 @@ public class CCuenta {
 
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
     }
 
     public double estado()
     {
-        return saldo;
+        return getSaldo();
     }
 
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
     public void retirar(double cantidad) throws Exception
@@ -35,6 +39,65 @@ public class CCuenta {
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
     }
+
+	/**
+	 * 
+	 * APARATADO 4
+	 * Encapsular los atributos de la clase CCuenta.
+	 *
+	 */
+	private String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the cuenta
+	 */
+	private String getCuenta() {
+		return cuenta;
+	}
+
+	/**
+	 * @param cuenta the cuenta to set
+	 */
+	private void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	/**
+	 * @return the saldo
+	 */
+	private double getSaldo() {
+		return saldo;
+	}
+
+	/**
+	 * @param saldo the saldo to set
+	 */
+	private void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	/**
+	 * @return the tipoInterés
+	 */
+	private double getTipoInterés() {
+		return tipoInterés;
+	}
+
+	/**
+	 * @param tipoInterés the tipoInterés to set
+	 */
+	private void setTipoInterés(double tipoInterés) {
+		this.tipoInterés = tipoInterés;
+	}
 }
--- Deposito/src/cuentas/Main.java
+++ Deposito/src/cuentas/Main.java
@@ -1,23 +1,51 @@
+package cuentas;
+/**
+ * 
+ * @author Lucas LMEL
+ * APARATADO 1
+ * Las clases deberán formar parte del paquete cuentas.
+ *
+ */
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+    	/**
+    	 * 
+    	 * APARATADO 3
+    	 * Introducir el método operativa_cuenta, 
+    	 * que englobe las sentencias de la clase Main que operan con el objeto cuenta1.
+    	 *
+    	 */
+        operativa_cuenta(0);
+    }
+
+	private static void operativa_cuenta(float cantidad) {
+    	/**
+    	 * 
+    	 * APARATADO 2
+    	 * Cambiar el nombre de la variable "miCuenta" por "cuenta1".
+    	 * 
+    	 * APARATADO 5
+    	 * Añadir un nuevo parámetro al método operativa_cuenta, 
+    	 * de nombre cantidad y de tipo float.
+    	 */
+		CCuenta cuenta1;
         double saldoActual;
 
-        miCuenta = new CCuenta("Antonio LÃ³pez","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
         try {
-            miCuenta.retirar(2300);
+            cuenta1.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            cuenta1.ingresar(695);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
-    }
+	}
 }
